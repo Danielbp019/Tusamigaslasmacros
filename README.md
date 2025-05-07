5654784422ff343fddc35c2f94d9db9291baddc6
Daniel Bautista P. <76835848+Danielbp019@users.noreply.github.com>
2025-05-06 21:43:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tusamigaslasmacros
+Guía web de creación de macros para el juego de World of Warcraft.
---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+// Buscador
+function filtrarIndice() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let items = document.querySelectorAll("#indice li");
+  let resultado = false;
+
+  items.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(input)) {
+      item.style.display = "list-item";
+      resultado = true;
+    } else {
+      item.style.display = "none";
+    }
+  });
+
+  let mensaje = document.getElementById("mensaje");
+  if (!resultado) {
+    mensaje.style.display = "block";
+    mensaje.innerText = "No se encontraron resultados.";
+  } else {
+    mensaje.style.display = "none";
+  }
+}
+
+// Cambio dia y noche script
+// Inicializar el tema guardado
+const savedTheme = localStorage.getItem("theme") || "dark";
+document.documentElement.setAttribute("data-bs-theme", savedTheme);
+document.getElementById("themeSwitch").checked = savedTheme === "dark";
+
+// Cambiar el tema al activar el switch
+document.getElementById("themeSwitch").addEventListener("change", function () {
+  const theme = this.checked ? "dark" : "light";
+  document.documentElement.setAttribute("data-bs-theme", theme);
+  localStorage.setItem("theme", theme);
+  updateThemeIcon(theme);
+});
+
+// Actualizar el icono SVG según el tema
+function updateThemeIcon(theme) {
+  const icon = document.querySelector(".theme-icon-active use");
+  if (theme === "dark") {
+    icon.setAttribute("href", "#moon-stars-fill");
+    document.querySelector(".theme-icon-active").classList.add("theme-dark");
+  } else {
+    icon.setAttribute("href", "#sun-fill");
+    document.querySelector(".theme-icon-active").classList.remove("theme-dark");
+  }
+}
+// Llamar a la función para actualizar el icono al cargar la página
+updateThemeIcon(savedTheme);
